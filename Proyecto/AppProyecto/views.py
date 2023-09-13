72525c3e91d3b37f5a2d2a1711b7eb536238fa88
Juan Menichetti <jpmenichetti13@gmail.com>
2023-09-13 17:05:34 -0300

    se agrego el formulario para agregar producto

--- Proyecto/AppProyecto/views.py
+++ Proyecto/AppProyecto/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from .models import *
+from .forms import FormProducto
 
 # Create your views here.
 
@@ -17,3 +18,20 @@ def producto(req):
 
 def categoria(req):
     return render (req, "categoria.html")
+
+def formProducto(req):
+    print('method',req.method)
+    print('POST',req.POST)
+
+    if req.method == 'POST':
+        miFormulario=FormProducto(req.POST)
+        if miFormulario.is_valid():
+            data=miFormulario.cleaned_data
+            producto=Producto(nombre=data['nombre'],descripcion=data['descripcion'],precio=data['precio'])
+            producto.save()
+
+        return render(req,"formProducto.html")
+    
+    else:
+        miFormulario=FormProducto()
+        return render(req, "formProducto.html",{"miFormulario":miFormulario})
